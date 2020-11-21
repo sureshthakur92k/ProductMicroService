53013c7f342b211e7b5a440d1a430b5a4ca539dc
Suresh Thakur <suresh.thakur92k@gmail.com>
2020-11-22 03:46:58 +0530

    Add project files.

---
+++ ProductMicroService/Repository/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using ProductMicroService.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductMicroService.Repository
+{
+   public interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+        Product GetProductById(int productId);
+        void InsertProduct(Product product);
+        void DeleteProduct(int ProductId);
+        void UpdateProduct(Product product);
+        void Save();
+    }
+}
---
+++ ProductMicroService/Repository/ProductRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductMicroService.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductMicroService.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ProductContext _dbContext;
+        public ProductRepository(ProductContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void DeleteProduct(int ProductId)
+        {
+            var product = _dbContext.Products.Find(ProductId);
+            _dbContext.Products.Remove(product);
+            Save();
+        }
+
+        public Product GetProductById(int productId)
+        {
+            return _dbContext.Products.Find(productId);
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return _dbContext.Products.ToList();
+        }
+
+        public void InsertProduct(Product product)
+        {
+            _dbContext.Add(product);
+            Save();
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _dbContext.Entry(product).State = EntityState.Modified;
+            Save();
+        }
+    }
+}
